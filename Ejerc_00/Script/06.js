321231bc658be29254054d1fd83f65f02c78e629
Naxalin <gonzialconerofdez@gmail.com>
2025-09-17 11:16:04 +0200

    Refactor course data structure and implement course summary calculation

--- Ejerc_00/Script/06.js
+++ Ejerc_00/Script/06.js
@@ -2,6 +2,24 @@
 let nombreCurso = ""
 let nombreProfesor = ""
 
+const estudiantes = {nombre : "", calificaciones: 0}
 
+const cursos = [nombreCurso, nombreProfesor, estudiantes]
+cursos.push({nombreCurso: "DWEC", nombreProfesor: "Gonzalo", estudiantes: estudiantes});
+cursos.push({nombreCurso: "DWES", nombreProfesor: "Manolo", estudiantes: estudiantes});
+cursos.push({nombreCurso: "DAW", nombreProfesor: "Nel", estudiantes: estudiantes});
 
-const cursos = []
+resumenCursos = cursos.map((promedio, cursos) => ({
+    ...promedio,
+    promedio: (estudiantes.calificaciones / estudiantes.length)
+}));
+
+cursosDestacados = resumenCursos.filters(function(resumenCursos){
+    if(resumenCursos.promedio >= 5){
+        return resumenCursos
+    }
+        })
+
+for (let i = 0; i < cursosDestacados.length; i++) {
+    console.log(`El curso ${cursosDestacados[i].nombreCurso}tiene un promedio de ${cursosDestacados[i].promedio} es uno de los destacados`)
+}
