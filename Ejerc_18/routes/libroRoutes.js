98f0b3341f03e61e52beb75cfc26a2649f0ee373
Naxalin <gonzialconerofdez@gmail.com>
2026-04-25 19:08:25 +0200

    fix: nombres de funciones en rutas corregidos

--- Ejerc_18/routes/libroRoutes.js
+++ Ejerc_18/routes/libroRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const libroController = require('../controllers/libroController');
 
-router.get('/', libroController.index);
-router.get('/libro/:id', libroController.detalle);
-router.get('/prestados', libroController.prestados);
-router.get('/vencidos', libroController.vencidos);
-router.get('/prestamos/usuario', libroController.usuario);
-router.get('/prestamo/formulario/:libro_id', libroController.formulario);
-router.post('/prestamo/nuevo', libroController.nuevoPrestamo);
-router.post('/prestamo/devolver/:libro_id', libroController.devolver);
+router.get('/libros', libroController.getAll);
+router.get('/libro/:id', libroController.getById);
+router.get('/prestados', libroController.getPrestados);
+router.get('/vencidos', libroController.getVencidos);
+router.get('/prestamos/usuario', libroController.getByUsuario);
+router.get('/prestamo/formulario/:libro_id', libroController.getFormulario);
+router.post('/prestamo/nuevo', libroController.createPrestamo);
+router.post('/prestamo/devolver/:libro_id', libroController.devolverPrestamo);
 
 module.exports = router;
